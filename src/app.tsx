9d010a61513776562da879e2e023f9d8f67d0375
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-04-01 16:37:47 -0300

    feat: add app file with variations of layouts based on routes

--- src/app.tsx
+++ src/app.tsx
@@ -1,23 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Header } from './components/header'
-import { BottomBar } from './components/bottom-bar'
+import { LayoutWithBottomBar } from './layouts/layout-with-bottombar'
+import { LayoutWithoutBottomBar } from './layouts/layout-without-bottombar'
 import { Home } from './pages/home'
 import { Training } from './pages/training'
+import { Login } from './pages/login'
+import { Cadastro } from './pages/register'
+import { Profile } from './pages/profile'
 
 export function App() {
-
   return (
-    <>
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/train' element={<Training />} />
-        {/* <Route path='/about' element={<About />} /> */}
-        {/* <Route path='/contact' element={<Contact />} /> */}
+        {/* Rotas sem a BottomBar */}
+        <Route element={<LayoutWithoutBottomBar />}>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/cadastro' element={<Cadastro />} />
+        </Route>
+
+        {/* Rotas com a BottomBar */}
+        <Route element={<LayoutWithBottomBar />}>
+          <Route path='/train' element={<Training />} />
+          <Route path='/profile' element={<Profile />} />
+        </Route>
       </Routes>
-      <BottomBar />
     </BrowserRouter>
-    </>
   )
 }
