188805d65635d77a12e4b0d792560030c45ab099
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-10-14 16:33:32 -0300

    refactor: change system logo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-57071a8e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kchvicf5pso"
+    "revision": "0.akbb6a1hck"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
