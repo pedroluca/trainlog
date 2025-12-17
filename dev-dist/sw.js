96867cc3b3a74b875fe33c0ce78a5beced9fc0d5
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-12-17 10:03:23 -0300

    refactor: adjust icons and manifest to better app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-57071a8e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vjq3qvhu0vg"
+    "revision": "0.scnv3tbasro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
