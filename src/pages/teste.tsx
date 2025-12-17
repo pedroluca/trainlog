4bf2b353ea8c6d1e90c0a7882db6135145667612
Pedro Luca Prates <pedrolucaofc@outlook.com>
2025-12-17 10:09:06 -0300

    chore: remove unused test file

--- src/pages/teste.tsx
+++
@@ -1,8 +0,0 @@
-// import { WorkoutCompleteModal } from '../components/workout-complete-modal'
-
-export function Teste() {
-  return (
-    // <WorkoutCompleteModal isOpen={true} onClose={() => {}} workoutName='Teste' />
-    <h1>teste</h1>
-  )
-}
