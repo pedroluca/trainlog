992023763654df32f5d75b619f8665a600d4bb4d
Pedro Luca Prates <pedrolucaofc@outlook.com>
2026-03-30 09:38:55 -0300

    refactor: adjust header on training screen to fit height

--- src/layouts/layout-with-bottombar.tsx
+++ src/layouts/layout-with-bottombar.tsx
@@ -1,11 +1,13 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import { Header } from '../components/header'
 import { BottomBar } from '../components/bottom-bar'
 
 export function LayoutWithBottomBar() {
+  const url = useLocation()
+  
   return (
     <main>
-      <Header />
+      <Header isFixed={url.pathname.startsWith('/train')} />
       <Outlet />
       <BottomBar />
     </main>
